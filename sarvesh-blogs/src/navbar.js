45ef052359e8979e490ef4406fd34e5603797a08
SarveshRajoba <sarvesh.rajoba10@gmail.com>
2024-09-11 00:57:04 +0530

    first commit

---
+++ sarvesh-blogs/src/navbar.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+
+const navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
+                    color: 'white',
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>New Blog</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default navbar;
+
+/*
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
+*/
